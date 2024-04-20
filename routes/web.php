791a781836de74b6d2fe653f99e6b4f82d28a44f
tuyetnhu06 <kimhuynhtuyetnhu0611@gmail.com>
2024-04-20 21:05:56 +0700

    Dang nhap, dang ky bang email, quan ly tai khoan thong tin KH

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+/*Cập nhật tài khoản */
+Route::get('/accountpanel','App\Http\Controllers\AccountUserController@accountpanel')
+->middleware('auth')->name("account");
+
+Route::post('/saveaccountinfo','App\Http\Controllers\AccountUserController@saveaccountinfo')
+->middleware('auth')->name('saveinfo');
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
 
 Route::get('/admin', function () { return redirect('/admin/dashboard'); });
 Route::get('/admin/dashboard','App\Http\Controllers\AdminController@dashboard')->name('admin.dashboard');
@@ -23,9 +37,5 @@ Route::get('/admin/products', 'App\Http\Controllers\AdminController@products')->
 Route::get('/admin/accounts', 'App\Http\Controllers\AdminController@accounts')->name('admin.accounts');
 Route::get('/admin/roles', 'App\Http\Controllers\AdminController@roles')->name('admin.roles');
 Route::get('/admin/dashboard/filter-by-date','App\Http\Controllers\AdminController@filter_by_date');
-
-
-
 Route::get('/','App\Http\Controllers\Controller@index')
 ->name('index');
-
