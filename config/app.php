870ae6c021bd09ad35a2f7ab2abc6ca33ef9ca59
dangquocviet34 <149297677+dangquocviet34@users.noreply.github.com>
2024-04-21 22:41:31 +0700

    export E, Pdf, chart

--- config/app.php
+++ config/app.php
@@ -231,8 +231,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'PDF' => Barryvdh\DomPDF\Facade::class,
-       
+      
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
 
