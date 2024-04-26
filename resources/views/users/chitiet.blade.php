103829dde9309dd4b20a1fa6a0f55e3ad07e4b01
dangquocviet34 <149297677+dangquocviet34@users.noreply.github.com>
2024-04-26 13:35:22 +0700

    chitietsanpham

--- resources/views/users/chitiet.blade.php
+++ resources/views/users/chitiet.blade.php
@@ -5,52 +5,65 @@
 
 
   <link type="text/css" rel="stylesheet" href="{{ asset('css/style.css') }}" />
+  @foreach($data as $sanpham)
   <!-- section -->
 	<div class="section">
 		<!-- container -->
 		<div class="container">
 			<!-- row -->
 			<div class="row">
+            <form >
+                @csrf
+                <input type="hidden" value="{{$sanpham->id_sanpham}}" class="cart_product_id_{{$sanpham->id_sanpham}}">
+                <input type="hidden" value="{{$sanpham->tensp}}" class="cart_product_name_{{$sanpham->id_sanpham}}">
+                <input type="hidden" value="{{$sanpham->image_sp}}" class="cart_product_image_{{$sanpham->id_sanpham}}">
+                <input type="hidden" value="{{$sanpham->discount_total}}" class="cart_product_price_{{$sanpham->id_sanpham}}">
+                <input type="hidden" value="1" class="cart_product_qty_{{$sanpham->id_sanpham}}">
+
 				<!--  Product Details -->
 				<div class="product product-details clearfix">
 					<div class="col-md-6">
 						<div id="product-main-view">
 							<div class="product-view">
-								<img src="./img/main-product01.jpg" alt="">
+								<img src="{{asset('images/'.$sanpham->image_sp)}}" alt="">
 							</div>
-							<div class="product-view">
-								<img src="./img/main-product02.jpg" alt="">
+                            <div class="product-view">
+								<img src="{{asset('images/'.$sanpham->image_sp)}}" alt="">
 							</div>
-							<div class="product-view">
-								<img src="./img/main-product03.jpg" alt="">
+                            <div class="product-view">
+								<img src="{{asset('images/'.$sanpham->image_sp)}}" alt="">
 							</div>
-							<div class="product-view">
-								<img src="./img/main-product04.jpg" alt="">
-							</div>
+							
 						</div>
 						<div id="product-view">
 							<div class="product-view">
-								<img src="./img/thumb-product01.jpg" alt="">
+								<img src="{{asset('images/'.$sanpham->image_sp)}}" alt="">
 							</div>
-							<div class="product-view">
-								<img src="./img/thumb-product02.jpg" alt="">
+                            <div class="product-view">
+								<img src="{{asset('images/'.$sanpham->image_sp)}}" alt="">
 							</div>
-							<div class="product-view">
-								<img src="./img/thumb-product03.jpg" alt="">
+                            <div class="product-view">
+								<img src="{{asset('images/'.$sanpham->image_sp)}}" alt="">
 							</div>
-							<div class="product-view">
-								<img src="./img/thumb-product04.jpg" alt="">
-							</div>
+							
 						</div>
 					</div>
 					<div class="col-md-6">
 						<div class="product-body">
 							<div class="product-label">
 								<span>New</span>
-								<span class="sale">-20%</span>
+                                @php 
+                                  if($sanpham->discount_value!=NULL)
+                                    $discount = intval($sanpham->discount_value);
+                                 else
+                                    $discount = 0;
+
+                                @endphp
+                                
+								<span class="sale">-{{$discount}}%</span>
 							</div>
-							<h2 class="product-name">Product Name Goes Here</h2>
-							<h3 class="product-price">$32.50 <del class="product-old-price">$45.00</del></h3>
+							<h2 class="product-name">{{$sanpham->tensp}}</h2>
+							<h3 class="product-price">{{number_format((float)$sanpham->discount_total,0,",",".")}}đ <del class="product-old-price">{{number_format((float)$sanpham->price,0,",",".")}}đ </del></h3>
 							<div>
 								<div class="product-rating">
 									<i class="fa fa-star"></i>
@@ -62,31 +75,36 @@
 								<a href="#">3 Review(s) / Add Review</a>
 							</div>
 							<p><strong>Availability:</strong> In Stock</p>
-							<p><strong>Brand:</strong> E-SHOP</p>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-								dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+							<p><strong>Xuất xứ:</strong> {{$sanpham->xuatxu}}</p>
+							<p>{{$sanpham->content}}</p>
+                          
 							<div class="product-options">
+                                @php
+                                 $datasize =  explode(',', $sanpham->sizess);
+
+
+                                @endphp
 								<ul class="size-option">
 									<li><span class="text-uppercase">Size:</span></li>
-									<li class="active"><a href="#">S</a></li>
-									<li><a href="#">XL</a></li>
-									<li><a href="#">SL</a></li>
+                                    @foreach($datasize as $datasize)
+                                        <li><a href="#">{{$datasize}}</a></li>
+
+                                    @endforeach
+									
 								</ul>
 								<ul class="color-option">
-									<li><span class="text-uppercase">Color:</span></li>
-									<li class="active"><a href="#" style="background-color:#475984;"></a></li>
-									<li><a href="#" style="background-color:#8A2454;"></a></li>
-									<li><a href="#" style="background-color:#BF6989;"></a></li>
-									<li><a href="#" style="background-color:#9A54D8;"></a></li>
+									<li><span class="text-uppercase">Color: <b>{{$sanpham->mausac}}</b></span></li>
+									
 								</ul>
 							</div>
 
 							<div class="product-btns">
 								<div class="qty-input">
 									<span class="text-uppercase">QTY: </span>
-									<input class="input" type="number">
+									<input class="input" type="number" value="1">
 								</div>
-								<button class="primary-btn add-to-cart"><i class="fa fa-shopping-cart"></i> Add to Cart</button>
+								<button type="button" class="primary-btn add-to-cart" data-id_product="{{$sanpham->id_sanpham}}" name="add-to-cart"><i class="fa fa-shopping-cart" ></i> Add to Car </button>
+									
 								<div class="pull-right">
 									<button class="main-btn icon-btn"><i class="fa fa-heart"></i></button>
 									<button class="main-btn icon-btn"><i class="fa fa-exchange"></i></button>
@@ -104,8 +122,7 @@
 							</ul>
 							<div class="tab-content">
 								<div id="tab1" class="tab-pane fade in active">
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-										irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+									<p>{{$sanpham->content}}</p>
 								</div>
 								<div id="tab2" class="tab-pane fade in">
 
@@ -213,109 +230,14 @@
 
 				</div>
 				<!-- /Product Details -->
+                </form>
 			</div>
 			<!-- /row -->
 		</div>
 		<!-- /container -->
 	</div>
 	<!-- /section -->
-@foreach($data as $sanpham)
-    <div class="text-center">
-    <div >
-    <div class="product product-details clearfix">
-                        <div class="col-md-6">
-                            <div id="product-main-view">
-                                <div class="product-view">
-                                    <img src="{{asset('images/'.$sanpham->image_sp)}}" alt="">
-                                    </div>
-                                    </div>
-                                    </div>
-                                
-                            
-    <div class="col-md-6">
-                            <div class="product-body">
-                                <div class="product-label">
-                                    <span>New</span>
-                                    <span class="sale"> </span> 
-                                    <!-- Giảm giá -->
-                                </div>
-                                <h2 class="product-name">{{$sanpham->tensp}}</h2> <!-- tên sản phẩm -->
-                                <h3 class="product-price"><!--giá mới --> <del class="product-old-price"><!-- giá cũ --></del></h3>
-                                <div>
-                                    <div class="product-rating">
-                                        <i class="fa fa-star"></i>
-                                        <i class="fa fa-star"></i>
-                                        <i class="fa fa-star"></i>
-                                        <i class="fa fa-star"></i>
-                                        <i class="fa fa-star-o empty"></i>
-                                    </div>
-                                    <a href="#">3 Review(s) / Add Review</a>
-                                </div>
-                                <p><strong>Giá</strong> {{$sanpham->price}}</p>
-                                <p><strong>Màu sắc</strong> {{$sanpham->mausac}}</p>
-                                <p><strong>Nhãn hiệu:</strong> G-SHOP</p>
-                                <p><strong>Xuất xứ:</strong> {{$sanpham->xuatxu}}</p>
-                                <p>{{$sanpham->content}}</p><br>
-                                <div class="product-options">
-                                    <ul class="size-option">
-                                        <li><span class="text-uppercase">Size:</span></li>
-                                        <li class="active"><a href="#">S</a></li>
-                                        <li><a href="#">XL</a></li>
-                                        <li><a href="#">SL</a></li>
-                                    </ul>
-                                    
-                                </div>
-
-                                <div class="product-btns">
-                                
-                                    <div class="qty-input">
-                                        <span class="text-uppercase">QTY: </span>
-                                        <input class="input" type="number"  id='product-number' size='5' min="1" value="1">
-                                    </div>
-                                    <button class="primary-btn add-to-cart" data-id="{{$sanpham->id_sanpham}}"><i class="fa fa-shopping-cart"></i> Thêm vào giỏ hàng</button>
-
-                                    <div class="pull-right">
-                                        <button class="main-btn icon-btn"><i class="fa fa-heart"></i></button>
-                                        <button class="main-btn icon-btn"><i class="fa fa-exchange"></i></button>
-                                        <button class="main-btn icon-btn"><i class="fa fa-share-alt"></i></button>
-                                    </div>
-                                </div>
-                            </div>
-        </div>
-        </div>
-    </div>
-    </div>
-    </div>
-
 @endforeach
-<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 
-<script>
-$(document).ready(function(){
-    $(".add-to-cart").click(function(){
-        var id_sp = $(this).data("id"); // Lấy id_sanpham từ thuộc tính dữ liệu của nút
-        var num = $("#product-number").val()
-        $.ajax({
-            type: "POST",
-            dataType: "json",
-            url: "{{route('cartadd')}}",
-            data: {
-                "_token": "{{ csrf_token() }}",
-                "id_sp": id_sp,
-                "num": num
-            },
-            beforeSend: function(){
-            },
-            success: function(data){
-                $("#cart-number-product").html(data);
-            },
-            error: function (xhr, status, error){
-            },
-            complete: function(xhr, status){
-            }
-        });
-    });
-});
-</script>
 
 </x-index-layout>
